c424ba14fceb5b7d93e82651c80bb2b68e33954a
Fabian Hafner <fabian.hafner@student.kit.edu>
2013-11-19 20:07:46 +0100

    small changes to the bats

--- pflichtenheft/winMake.bat
+++ pflichtenheft/winMake.bat
@@ -1,6 +1,6 @@
 java -jar Umlet/umlet.jar -action=convert -format=pdf -filename=Paketdiagramm.uxf -output=Paketdiagramm
-pdflatex -interaction nonstopmode dokument.tex
+pdflatex -interaction nonstopmode -file-line-error dokument.tex
 makeglossaries dokument
-pdflatex -interaction nonstopmode dokument.tex
+pdflatex -interaction nonstopmode -file-line-error dokument.tex
 makeglossaries dokument
-pdflatex -interaction nonstopmode dokument.tex
+pdflatex -interaction nonstopmode -file-line-error dokument.tex
