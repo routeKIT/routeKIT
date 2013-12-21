595abbfc737ac603ca70140269a8e3378608d222
Anastasia Zinkina <uajzt@student.kit.edu>
2013-12-21 22:02:11 +0100

    Created "About" window.

--- src/views/AboutView.java
+++ src/views/AboutView.java
@@ -1,6 +1,21 @@
 package views;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 /**
  * Zeigt das Fenster mit den Informationen über routeKIT auf dem Bildschirm an.
  */
-public class AboutView {
+public class AboutView extends JFrame {
+	public AboutView() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(400, 400);
+
+		JPanel panel = new JPanel();
+		setVisible(true);
+
+	}
+	public static void main(String[] args) {
+		new AboutView();
+	}
 }
