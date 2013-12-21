17d9bba615e08b4d2bfce1069e739ff3cbbb460a
Anastasia Zinkina <uajzt@student.kit.edu>
2013-12-21 22:12:11 +0100

    OK-Button, Text

--- src/views/AboutView.java
+++ src/views/AboutView.java
@@ -1,6 +1,11 @@
 package views;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JPanel;
 
 /**
@@ -11,9 +16,37 @@ public class AboutView extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(400, 400);
 
-		JPanel panel = new JPanel();
+		JPanel panel = new JPanel(new BorderLayout());
+		panel.setBackground(Color.WHITE);
+
+		JPanel text = initTextPane();
+		JPanel ok = initOkPane();
+
+		panel.add(text, BorderLayout.CENTER);
+		panel.add(ok, BorderLayout.SOUTH);
+
+		setContentPane(panel);
 		setVisible(true);
 
+	}
+	private JPanel initTextPane() {
+		JPanel text = new JPanel(new BorderLayout());
+		JList<String> information = new JList<String>(new String[]{
+				"routeKIT, Version 0.1", "PSE-Projekt WS 2013/14",
+				"KIT, Institut für Theoretische Informatik", "Algorithmik II",
+				"Prof. Dr. Peter Sanders", "Julian Arz", "G. Veit Batz",
+				"Dr. Dennis Luxen", "Dennis Schieferdecker",
+				"Ⓒ 2013-2014 Kevin Birke, Felix Dörre, Fabian Hafner,",
+				"Lucas Werkmeister, Dominic Ziegler, Anastasia Zinkina"});
+		text.add(information, BorderLayout.CENTER);
+		return text;
+	}
+
+	private JPanel initOkPane() {
+		JPanel ok = new JPanel();
+		ok.add(new JButton("OK"));
+		ok.setBackground(Color.WHITE);
+		return ok;
 	}
 	public static void main(String[] args) {
 		new AboutView();
