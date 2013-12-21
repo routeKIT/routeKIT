9793b11c77e994828167f797b0a17872f128531a
Anastasia Zinkina <uajzt@student.kit.edu>
2013-12-21 22:25:09 +0100

    Schriftart geändert.

--- src/views/AboutView.java
+++ src/views/AboutView.java
@@ -2,6 +2,7 @@ package views;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -13,35 +14,38 @@ import javax.swing.JPanel;
  */
 public class AboutView extends JFrame {
 	public AboutView() {
+		super("About");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setSize(400, 400);
-
+		setSize(400, 350);
+		
 		JPanel panel = new JPanel(new BorderLayout());
 		panel.setBackground(Color.WHITE);
-
+		
 		JPanel text = initTextPane();
 		JPanel ok = initOkPane();
-
+		
 		panel.add(text, BorderLayout.CENTER);
 		panel.add(ok, BorderLayout.SOUTH);
-
+		
 		setContentPane(panel);
 		setVisible(true);
-
-	}
+		
+	}	
 	private JPanel initTextPane() {
 		JPanel text = new JPanel(new BorderLayout());
 		JList<String> information = new JList<String>(new String[]{
-				"routeKIT, Version 0.1", "PSE-Projekt WS 2013/14",
+				"routeKIT, Version 0.1", "PSE-Projekt WS 2013/14", 
 				"KIT, Institut für Theoretische Informatik", "Algorithmik II",
 				"Prof. Dr. Peter Sanders", "Julian Arz", "G. Veit Batz",
-				"Dr. Dennis Luxen", "Dennis Schieferdecker",
-				"Ⓒ 2013-2014 Kevin Birke, Felix Dörre, Fabian Hafner,",
+				"Dr. Dennis Luxen", "Dennis Schieferdecker", 
+				"Ⓒ 2013-2014 Kevin Birke, Felix Dörre, Fabian Hafner,", 
 				"Lucas Werkmeister, Dominic Ziegler, Anastasia Zinkina"});
+		Font displayFont = new Font("Serif", Font.LAYOUT_LEFT_TO_RIGHT, 15);
+		information.setFont(displayFont);
 		text.add(information, BorderLayout.CENTER);
 		return text;
 	}
-
+	
 	private JPanel initOkPane() {
 		JPanel ok = new JPanel();
 		ok.add(new JButton("OK"));
