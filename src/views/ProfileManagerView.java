a7c9bd0064b75793d004c2a09e4c158639760a28
Anastasia Zinkina <uajzt@student.kit.edu>
2013-12-21 22:39:07 +0100

    Window for profile manager

--- src/views/ProfileManagerView.java
+++ src/views/ProfileManagerView.java
@@ -1,11 +1,32 @@
 package views;
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import profiles.Profile;
 /**
  * Zeigt das Fenster der Profilverwaltung auf dem Bildschirm an.
  */
-public class ProfileManagerView {
+public class ProfileManagerView extends JFrame {
+	public ProfileManagerView() {
+		super("Profilverwaltung");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(500, 400);
+		setLocationRelativeTo(getParent());
+		setResizable(false);
+
+		JPanel contentPane = new JPanel(new BorderLayout());
+		contentPane.setBackground(Color.WHITE);
+
+		setContentPane(contentPane);
+		setVisible(true);
+	}
+	public static void main(String[] args) {
+		new ProfileManagerView();
+	}
 	/**
 	 * Setzt das aktuelle Profil auf das angegebene Profil, lädt seine Werte in
 	 * die Eingabefelder und aktiviert/deaktiviert die Eingabeelemente, je
