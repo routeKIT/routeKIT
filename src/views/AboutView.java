914b1a66c16e8bc6725448dfb5fb5db184cf1be1
Anastasia Zinkina <uajzt@student.kit.edu>
2013-12-22 00:20:57 +0100

    About: new layout. Text centered.

--- src/views/AboutView.java
+++ src/views/AboutView.java
@@ -6,8 +6,11 @@ import java.awt.Font;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JTextPane;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 
 /**
  * Zeigt das Fenster mit den Informationen über routeKIT auf dem Bildschirm an.
@@ -16,42 +19,52 @@ public class AboutView extends JFrame {
 	public AboutView() {
 		super("Über");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setSize(400, 350);
-		
+		setSize(500, 350);
+		setLocationRelativeTo(getParent());
+		setResizable(false);
+
 		JPanel panel = new JPanel(new BorderLayout());
 		panel.setBackground(Color.WHITE);
-		
+
 		JPanel text = initTextPane();
 		JPanel ok = initOkPane();
-		
+
 		panel.add(text, BorderLayout.CENTER);
 		panel.add(ok, BorderLayout.SOUTH);
-		
+
 		setContentPane(panel);
 		setVisible(true);
-		
-	}	
+	}
+
 	private JPanel initTextPane() {
 		JPanel text = new JPanel(new BorderLayout());
-		JList<String> information = new JList<String>(new String[]{
-				"routeKIT, Version 0.1", "PSE-Projekt WS 2013/14", 
-				"KIT, Institut für Theoretische Informatik", "Algorithmik II",
-				"Prof. Dr. Peter Sanders", "Julian Arz", "G. Veit Batz",
-				"Dr. Dennis Luxen", "Dennis Schieferdecker", 
-				"Ⓒ 2013-2014 Kevin Birke, Felix Dörre, Fabian Hafner,", 
-				"Lucas Werkmeister, Dominic Ziegler, Anastasia Zinkina"});
-		Font displayFont = new Font("Serif", Font.LAYOUT_LEFT_TO_RIGHT, 15);
+		JTextPane information = new JTextPane();
+		information.setText("routeKIT, Version 0.1" + "\n"
+				+ "PSE-Projekt WS 2013/14"
+				+ "KIT, Institut für Theoretische Informatik" + "\n"
+				+ "Algorithmik II" + "\n" + "Prof. Dr. Peter Sanders" + "\n"
+				+ "Julian Arz" + "\n" + "G. Veit Batz" + "\n"
+				+ "Dr. Dennis Luxen" + "\n" + "Dennis Schieferdecker" + "\n"
+				+ "Ⓒ 2013-2014 Kevin Birke, Felix Dörre, Fabian Hafner," + "\n"
+				+ "Lucas Werkmeister, Dominic Ziegler, Anastasia Zinkina");
+		Font displayFont = new Font("Serif", Font.ROMAN_BASELINE, 18);
+		StyledDocument doc = information.getStyledDocument();
+		SimpleAttributeSet center = new SimpleAttributeSet();
+		StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
+		doc.setParagraphAttributes(0, doc.getLength(), center, false);
+
 		information.setFont(displayFont);
 		text.add(information, BorderLayout.CENTER);
 		return text;
 	}
-	
+
 	private JPanel initOkPane() {
 		JPanel ok = new JPanel();
 		ok.add(new JButton("OK"));
 		ok.setBackground(Color.WHITE);
 		return ok;
 	}
+
 	public static void main(String[] args) {
 		new AboutView();
 	}
