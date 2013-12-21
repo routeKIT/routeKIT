04b283b710acc6df66cd44b5f8d6cbb7211443b2
Anastasia Zinkina <uajzt@student.kit.edu>
2013-12-21 22:53:10 +0100

    Profile manager the Buttons "Löschen" and "Neu". Combobox for names.

--- src/views/ProfileManagerView.java
+++ src/views/ProfileManagerView.java
@@ -1,9 +1,14 @@
 package views;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.util.List;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JPanel;
 
 import profiles.Profile;
@@ -14,15 +19,48 @@ public class ProfileManagerView extends JFrame {
 	public ProfileManagerView() {
 		super("Profilverwaltung");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setSize(500, 400);
+		setSize(600, 400);
 		setLocationRelativeTo(getParent());
 		setResizable(false);
 
 		JPanel contentPane = new JPanel(new BorderLayout());
-		contentPane.setBackground(Color.WHITE);
-
+		
+		JPanel north = initNorthPane();
+		JPanel center = initCenterPane();
+		JPanel south = initSouthPane();
+		
+		contentPane.add(north, BorderLayout.NORTH);
+		contentPane.add(south, BorderLayout.SOUTH);
+		contentPane.add(center, BorderLayout.CENTER);
+		
 		setContentPane(contentPane);
 		setVisible(true);
+	}
+	private JPanel initNorthPane() {
+		JPanel north = new JPanel(new FlowLayout());
+		north.setBackground(Color.WHITE);
+		
+		JList<String> profile = new JList<String>(new String[]{"Proﬁl auswählen:"});
+		JComboBox<Object> profilename = new JComboBox<>();
+		profilename.setMinimumSize(new Dimension(250, 26));
+		profilename.setPreferredSize(new Dimension(250, 26));
+		
+		north.add(profile);
+		north.add(profilename);
+		north.add(new JButton("Löschen"));
+		north.add(new JButton("Neu"));
+		
+		return north;
+	}
+	
+	private JPanel initSouthPane() {
+		JPanel south = new JPanel();
+		return south;
+	}
+	
+	private JPanel initCenterPane() {
+		JPanel center = new JPanel();
+		return center;
 	}
 	public static void main(String[] args) {
 		new ProfileManagerView();
