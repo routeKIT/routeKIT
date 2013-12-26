473709d9bae1a170b8feb6c85db157d993eab8cd
Felix Dörre <felix.doerre@student.kit.edu>
2013-12-26 01:34:53 +0100

    Impl: Weights

--- src/edu/kit/pse/ws2013/routekit/models/Weights.java
+++ src/edu/kit/pse/ws2013/routekit/models/Weights.java
@@ -1,5 +1,9 @@
 package edu.kit.pse.ws2013.routekit.models;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 /**
  * Enthält die Kantengewichte für den vorberechneten Graphen.
  */
@@ -28,5 +32,19 @@ public class Weights {
 	 */
 	public int getWeight(int turn) {
 		return weights[turn];
+	}
+	
+	public void save(DataOutputStream os) throws IOException{
+		os.writeInt(weights.length);
+		for (int i = 0; i < weights.length; i++) {
+			os.writeInt(weights[i]);
+		}
+	}
+	public static Weights load(DataInputStream is) throws IOException{
+		int[] weights = new int[is.readInt()];
+		for (int i = 0; i < weights.length; i++) {
+			weights[i] = is.readInt();
+		}
+		return new Weights(weights);
 	}
 }
