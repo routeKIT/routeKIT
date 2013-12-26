850dcd4103462cf8ae39fde1519c45f564b773e3
Felix Dörre <felix.doerre@student.kit.edu>
2013-12-26 02:14:23 +0100

    Impl: Simple Tile cache (no pre-rendering) + TestCase

--- src/edu/kit/pse/ws2013/routekit/mapdisplay/TileCache.java
+++ src/edu/kit/pse/ws2013/routekit/mapdisplay/TileCache.java
@@ -1,5 +1,9 @@
 package edu.kit.pse.ws2013.routekit.mapdisplay;
 import java.awt.image.BufferedImage;
+import java.lang.ref.SoftReference;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.concurrent.LinkedBlockingDeque;
 /**
  * Verwaltet die Berechnung von Kartenkacheln und ist ein Zwischenspeicher für
  * diese. Kacheln können angefragt werden, und nachdem die (asynchrone)
@@ -11,6 +15,48 @@ import java.awt.image.BufferedImage;
  * {@code SoftReference}s).
  */
 public class TileCache {
+	private class TileJob implements Runnable {
+		private int x;
+		private int y;
+		private int zoom;
+
+		public TileJob(int x, int y, int zoom) {
+			this.x = x;
+			this.y = y;
+			this.zoom = zoom;
+		}
+
+		@Override
+		public void run() {
+			BufferedImage result = target.renderTile(x, y, zoom);
+			map.put(key(x, y, zoom), new SoftReference<BufferedImage>(result));
+			fireListeners(x, y, zoom, result);
+		}
+	}
+	private class Worker extends Thread {
+		@Override
+		public void run() {
+			while (running) {
+				while (waiting.size() > 100) {// too much work
+					waiting.removeLast();
+				}
+				try {
+					waiting.takeFirst().run();
+				} catch (InterruptedException e) {
+					// its ok...
+				}
+			}
+		}
+	}
+	private boolean running = true;
+	private LinkedBlockingDeque<TileJob> waiting = new LinkedBlockingDeque<>();
+
+	private LinkedList<TileFinishedListener> listeners = new LinkedList<>();
+	private TileSource target;
+	private HashMap<String, SoftReference<BufferedImage>> map = new HashMap<>();
+
+	private Worker worker;
+
 	/**
 	 * Konstruktor: Erstellt einen neuen Cache für die angegebene
 	 * {@link TileSource}.
@@ -20,6 +66,9 @@ public class TileCache {
 	 *            durchführt und deren Ergebnisse zwischengespeichert werden.
 	 */
 	public TileCache(TileSource target) {
+		this.target = target;
+		worker = new Worker();
+		worker.start();
 	}
 	/**
 	 * Ist die angeforderte Kachel bereits im Zwischenspeicher vorhanden, so
@@ -30,14 +79,20 @@ public class TileCache {
 	 * angefordert und im Zwischenspeicher gespeichert.
 	 * 
 	 * @param x
-	 *            Die -X-Komponente.
+	 *            Die SMT-X-Komponente.
 	 * @param y
-	 *            Die -Y-Komponente.
+	 *            Die SMT-Y-Komponente.
 	 * @param zoom
 	 *            Die Zoomstufe.
 	 * @return
 	 */
 	public BufferedImage renderTile(int x, int y, int zoom) {
+		String key = key(x, y, zoom);
+		SoftReference<BufferedImage> cacheVal = map.get(key);
+		if (cacheVal != null && cacheVal.get() != null) {
+			return cacheVal.get();
+		}
+		waiting.addFirst(new TileJob(x, y, zoom));
 		return null;
 	}
 	/**
@@ -48,5 +103,35 @@ public class TileCache {
 	 *            Der Listener, der hinzugefügt werden soll.
 	 */
 	public void addTileFinishedListener(TileFinishedListener listener) {
+		listeners.add(listener);
+	}
+
+	private void fireListeners(int x, int y, int zoom, BufferedImage tile) {
+		for (TileFinishedListener list : listeners) {
+			list.tileFinished(x, y, zoom, tile);
+
+		}
+	}
+	private static String key(int x, int y, int zoom) {
+		return x + "/" + y + "/" + zoom;
+	}
+
+	/**
+	 * Causes this TileCache to stop fetching.
+	 */
+	public void stop() {
+		running = false;
+		worker.interrupt();
+	}
+
+	/**
+	 * Causes this TileCache to stop fetching and waits for total termination.
+	 * 
+	 * @throws InterruptedException
+	 *             if the waiting is interrupted. if tw
+	 */
+	public void waitForStop() throws InterruptedException {
+		stop();
+		worker.join();
 	}
 }
---
+++ test/routeKIT/test/mapdisplay/TestTileCache.java
@@ -0,0 +1,78 @@
+package routeKIT.test.mapdisplay;
+
+import java.awt.image.BufferedImage;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import edu.kit.pse.ws2013.routekit.mapdisplay.TileCache;
+import edu.kit.pse.ws2013.routekit.mapdisplay.TileFinishedListener;
+import edu.kit.pse.ws2013.routekit.mapdisplay.TileSource;
+
+public class TestTileCache {
+	class WaitingSource implements TileSource {
+
+		@Override
+		public BufferedImage renderTile(int x, int y, int zoom) {
+			synchronized (TestTileCache.this) {
+				return new BufferedImage(1, 1, BufferedImage.TYPE_4BYTE_ABGR);
+			}
+		}
+
+	}
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	Throwable failed = null;
+	@Test
+	public void test() throws Throwable {
+		final TileCache tc = new TileCache(new WaitingSource());
+		tc.addTileFinishedListener(new TileFinishedListener() {
+			int last = -1;
+
+			@Override
+			public void tileFinished(int x, int y, int zoom, BufferedImage tile) {
+				try {
+					if (last == -1) {
+						assertEquals(0, x);
+						last = 120;
+					}else if(last > 20){
+						last--;
+						assertEquals(last, x);
+					}
+					assertEquals(tile, tc.renderTile(x, y, zoom));
+				} catch (Throwable t) {
+					failed = t;
+				}
+			}
+		});
+		synchronized (this) {
+			assertNull(tc.renderTile(0, 0, 19));
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			for (int i = 1; i < 120; i++) {
+				assertNull(tc.renderTile(i, 0, 19));
+			}
+		}
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		assertNotNull(tc.renderTile(0, 0, 19));
+		assertNull(tc.renderTile(1, 0, 19));
+		assertNull(tc.renderTile(2, 0, 19));
+		assertNull(tc.renderTile(3, 0, 19));
+		assertNotNull(tc.renderTile(50, 0, 19));
+		tc.waitForStop();
+		if (failed != null) {
+			throw failed;
+		}
+	}
+
+}
