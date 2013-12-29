7eb87315880f0edb15a5427cb2fb9ce7c7e7e860
Kevin Birke <kevin.birke@student.kit.edu>
2013-12-29 19:35:39 +0100

    ArcFlagsDijkstra benutzt nun das Interface RouteCalculator

--- src/edu/kit/pse/ws2013/routekit/routecalculation/ArcFlagsDijkstra.java
+++ src/edu/kit/pse/ws2013/routekit/routecalculation/ArcFlagsDijkstra.java
@@ -1,8 +1,19 @@
 package edu.kit.pse.ws2013.routekit.routecalculation;
+
+import edu.kit.pse.ws2013.routekit.models.ProfileMapCombination;
+import edu.kit.pse.ws2013.routekit.util.PointOnEdge;
+
 /**
  * Verwendet Dijkstra’s Algorithmus, um die schnellste Route zwischen Start- und
  * Zielpunkt für die aktuelle Kombination aus Karte und Profil zu berechnen.
  * Durch Arc-Flags wird die Berechnung beschleunigt.
  */
-public class ArcFlagsDijkstra {
+public class ArcFlagsDijkstra implements RouteCalculator {
+
+	@Override
+	public Route calculateRoute(PointOnEdge start, PointOnEdge destination,
+			ProfileMapCombination data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
