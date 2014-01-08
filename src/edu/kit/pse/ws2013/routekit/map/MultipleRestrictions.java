ce534cd0e774f94cea7354e708f73ac97d6a4687
Dominic Ziegler <dominic.ziegler@student.kit.edu>
2014-01-08 23:37:46 +0100

    Implement the MultipleRestrictions class

--- src/edu/kit/pse/ws2013/routekit/map/MultipleRestrictions.java
+++ src/edu/kit/pse/ws2013/routekit/map/MultipleRestrictions.java
@@ -1,20 +1,57 @@
 package edu.kit.pse.ws2013.routekit.map;
 
-import java.util.Set;
+import java.util.Collection;
+
+import edu.kit.pse.ws2013.routekit.profiles.Profile;
 
 /**
- * Repräsentiert eine Kombination aus mehreren Beschränkungen.
+ * A combination of multiple restrictions.
  */
-public class MultipleRestrictions {
+public class MultipleRestrictions implements Restriction {
+	private static final Restriction nullInstance = new Restriction() {
+		@Override
+		public boolean allows(Profile p) {
+			return true;
+		}
+	};
+
 	/**
-	 * (statisch) Gibt eine Instanz dieser Klasse für den angegebenen
-	 * Fahrzeugtyp zurück.
+	 * Returns an instance of a {@link Restriction} for the specified
+	 * restrictions.
 	 * 
 	 * @param restrictions
-	 *            Eine Menge von Beschränkungen.
-	 * @return
+	 *            a set of restrictions
+	 * @return the desired instance
+	 * @throws IllegalArgumentException
+	 *             if {@code restrictions} is {@code null}
 	 */
-	public Restriction getInstance(Set<Restriction> restrictions) {
-		return null;
+	public Restriction getInstance(Collection<Restriction> restrictions) {
+		if (restrictions == null) {
+			throw new IllegalArgumentException();
+		}
+
+		if (restrictions.isEmpty()) {
+			return nullInstance;
+		}
+		if (restrictions.size() == 1) {
+			return restrictions.iterator().next();
+		}
+		return new MultipleRestrictions(restrictions);
+	}
+
+	private final Restriction[] restrictions;
+
+	private MultipleRestrictions(Collection<Restriction> restrictions) {
+		this.restrictions = restrictions.toArray(this.restrictions);
+	}
+
+	@Override
+	public boolean allows(Profile profile) {
+		for (Restriction r : restrictions) {
+			if (!r.allows(profile)) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
