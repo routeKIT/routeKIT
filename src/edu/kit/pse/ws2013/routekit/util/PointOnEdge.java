f88a9167befd5eed95683117766b8bcadef81aef
Dominic Ziegler <dominic.ziegler@student.kit.edu>
2014-01-09 21:38:06 +0100

    Implement PointOnEdge

--- src/edu/kit/pse/ws2013/routekit/util/PointOnEdge.java
+++ src/edu/kit/pse/ws2013/routekit/util/PointOnEdge.java
@@ -1,17 +1,51 @@
 package edu.kit.pse.ws2013.routekit.util;
 
 /**
- * Beschreibt einen Punkt auf der Kante.
+ * A point on an edge.
  */
 public class PointOnEdge {
+	private int edge;
+	private float position;
+
 	/**
-	 * Konstruktor: Erstellt ein neues Objekt aus den gegebenen Attributen.
+	 * Creates a new object with the given attributes.
 	 * 
 	 * @param edge
-	 *            Die Kante.
+	 *            the edge
 	 * @param position
-	 *            Der Wert für das Attribut {@code position}.
+	 *            a value between 0 and 1 representing the share of the distance
+	 *            between the start of the edge and the point in the total
+	 *            length of the edge
+	 * @throws IllegalArgumentException
+	 *             if {@code edge} is negative or {@code position} is an invalid
+	 *             value
 	 */
 	public PointOnEdge(int edge, float position) {
+		if (edge < 0 || position < 0 || position > 1) {
+			throw new IllegalArgumentException();
+		}
+
+		this.edge = edge;
+		this.position = position;
+	}
+
+	/**
+	 * Returns the edge on which this point lies.
+	 * 
+	 * @return the edge
+	 */
+	public int getEdge() {
+		return edge;
+	}
+
+	/**
+	 * Returns the position of this point on the edge.
+	 * 
+	 * @return a value between 0 and 1 representing the share of the distance
+	 *         between the start of the edge and this point in the total length
+	 *         of the edge
+	 */
+	public float getPosition() {
+		return position;
 	}
 }
