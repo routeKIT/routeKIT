7b6abdfcd509e65b7e79a1207592e23561def54b
Dominic Ziegler <dominic.ziegler@student.kit.edu>
2014-01-13 22:44:09 +0100

    Implement the MapEdge class

--- src/edu/kit/pse/ws2013/routekit/precalculation/MapEdge.java
+++ src/edu/kit/pse/ws2013/routekit/precalculation/MapEdge.java
@@ -1,18 +1,41 @@
 package edu.kit.pse.ws2013.routekit.precalculation;
 
 /**
- * Stellt eine Kante im Straßengraphen dar. Dies ist nur eine vom
- * {@link OSMParser} verwendete Zwischendarstellung.
+ * This class represents an edge in the street map graph. This is only a
+ * temporary representation used by the {@link OSMParser}.
  */
 public class MapEdge {
+	private int targetNode;
+	private OSMWay way;
+
 	/**
-	 * Konstruktor: Erzeugt ein neues Objekt mit den angegebenen Attributen.
+	 * Creates a new object with the given attributes.
 	 * 
-	 * @param target
-	 *            Die ID des Zielknotens.
+	 * @param targetNode
+	 *            the OSM node identifier of the target node
 	 * @param way
-	 *            Der zur Kante gehörige OSM-Weg.
+	 *            the OSM way to which the edge belongs
 	 */
-	public MapEdge(int target, OSMWay way) {
+	public MapEdge(int targetNode, OSMWay way) {
+		this.targetNode = targetNode;
+		this.way = way;
+	}
+
+	/**
+	 * Returns the OSM node identifier of the target node.
+	 * 
+	 * @return the ID of the target node
+	 */
+	public int getTargetNode() {
+		return targetNode;
+	}
+
+	/**
+	 * Returns the OSM way to which this edge belongs.
+	 * 
+	 * @return the OSM way
+	 */
+	public OSMWay getWay() {
+		return way;
 	}
 }
