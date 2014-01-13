3116a137b6676ffa255b47e442cb84794f6e516d
Kevin Birke <kevin.birke@student.kit.edu>
2014-01-13 14:37:12 +0100

    ArcFlagsDijkstra Dummy mit besserer Rückgabe erweitert

--- src/edu/kit/pse/ws2013/routekit/routecalculation/ArcFlagsDijkstra.java
+++ src/edu/kit/pse/ws2013/routekit/routecalculation/ArcFlagsDijkstra.java
@@ -2,6 +2,7 @@ package edu.kit.pse.ws2013.routekit.routecalculation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import edu.kit.pse.ws2013.routekit.models.ProfileMapCombination;
 import edu.kit.pse.ws2013.routekit.util.PointOnEdge;
@@ -16,11 +17,34 @@ public class ArcFlagsDijkstra implements RouteCalculator {
 	@Override
 	public Route calculateRoute(PointOnEdge start, PointOnEdge destination,
 			ProfileMapCombination data) {
-		
+
+		// List<Integer> turns = new ArrayList<Integer>();
+
+		// Route route = new Route(data, start, destination, turns);
+
+		Route route = calculateDummy(start, destination, data);
+
+		return route;
+	}
+
+	private Route calculateDummy(PointOnEdge start, PointOnEdge destination,
+			ProfileMapCombination data) {
 		List<Integer> turns = new ArrayList<Integer>();
-		
+		turns.add(start.getEdge());
+
+		Set<Integer> eins = data.getStreetMap().getEdgeBasedGraph()
+				.getOutgoingTurns(start.getEdge());
+
+		for (Integer integer : eins) {
+			int turn = data.getStreetMap().getEdgeBasedGraph()
+					.getTargetEdge(integer);
+			turns.add(turn);
+		}
+
+		// turns.add(data.getStreetMap().getEdgeBasedGraph().getTargetEdge(data.getStreetMap().getEdgeBasedGraph().getOutgoingTurns(start.getEdge()).iterator().next()));
+
 		Route route = new Route(data, start, destination, turns);
-		
+
 		return route;
 	}
 }
