8758d2a72b64f07be62a2c9746aa99914f8b2207
Lucas Werkmeister <lucas.werkmeister@student.kit.edu>
2014-01-13 18:37:26 +0100

    Impl: MapManager: implement fully

--- src/edu/kit/pse/ws2013/routekit/controllers/MapManager.java
+++ src/edu/kit/pse/ws2013/routekit/controllers/MapManager.java
@@ -1,63 +1,126 @@
 package edu.kit.pse.ws2013.routekit.controllers;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 import edu.kit.pse.ws2013.routekit.map.StreetMap;
 
 /**
- * Verwaltet die Kartendaten. Hat intern eine Menge von vorhandenen Kartendaten.
+ * Manages the street maps and saves/loads them to/from disk.
  */
 public class MapManager {
 
 	private static MapManager instance;
 
-	private MapManager() {
-		// TODO implement
+	private final File root;
+	private final Map<StreetMap, File> maps;
+
+	private MapManager(File root) {
+		this.root = root;
+		this.maps = new HashMap<>();
+		if (!root.isDirectory()) {
+			throw new IllegalArgumentException(root.toString()
+					+ " is not a directory!");
+		}
+		for (String childString : root.list()) {
+			File child = new File(root, childString);
+			if (child.isDirectory()) {
+				maps.put(StreetMap.loadLazily(child), child);
+			}
+		}
 	}
 
 	/**
-	 * Löscht die ausgewählte Karte aus der internen Liste und von der
-	 * Festplatte (die zwei Graphen, Daten über Beschränkungen und alle
-	 * Vorberechnung).
+	 * Removes the given map from the internal list and deletes it from disk.
 	 * 
 	 * @param map
-	 *            Die Karte, die gelöscht werden soll.
+	 *            The map that shall be deleted.
 	 */
 	public void deleteMap(StreetMap map) {
+		try {
+			// recursive directory delete:
+			// http://stackoverflow.com/a/8685959/1420237
+			Files.walkFileTree(maps.get(map).toPath(),
+					new SimpleFileVisitor<Path>() {
+						@Override
+						public FileVisitResult visitFile(Path file,
+								BasicFileAttributes attrs) throws IOException {
+							Files.delete(file);
+							return FileVisitResult.CONTINUE;
+						}
+
+						@Override
+						public FileVisitResult visitFileFailed(Path file,
+								IOException exc) throws IOException {
+							Files.delete(file);
+							return FileVisitResult.CONTINUE;
+						}
+
+						@Override
+						public FileVisitResult postVisitDirectory(Path dir,
+								IOException exc) throws IOException {
+							if (exc == null) {
+								Files.delete(dir);
+								return FileVisitResult.CONTINUE;
+							} else {
+								throw exc;
+							}
+						}
+					});
+		} catch (IOException e) {
+
+		}
+		maps.remove(map);
 	}
 
 	/**
-	 * Gibt alle Karte in der internen Liste zurück.
-	 * 
-	 * @return
-	 */
-	public Set<StreetMap> getMaps() {
-		return null;
-	}
-
-	/**
-	 * Speichert die ausgewählte Karte in der internen Liste und auf der
-	 * Festplatte.
-	 * 
-	 * (Der Speicherort wird vom Manager deckend verwaltet.)
+	 * Adds the given map to the internal list and saves it on disk.
 	 * 
 	 * @param map
-	 *            Die Karte, die gespeichert werden soll.
+	 *            The map that shall be saved.
+	 * @throws IOException
 	 */
-	public void saveMap(StreetMap map) {
+	public void saveMap(StreetMap map) throws IOException {
+		File f = maps.get(map);
+		if (f == null) {
+			f = new File(root, map.getName());
+			f.mkdir();
+		}
+		map.save(f);
+		maps.put(map, f);
+	}
+
+	/**
+	 * Returns all maps in the internal list.
+	 * 
+	 * @return All maps.
+	 */
+	public Set<StreetMap> getMaps() {
+		return maps.keySet();
 	}
 
 	/**
 	 * Initializes the {@link MapManager}.
 	 * 
+	 * @param rootDirectory
+	 *            The directory that contains all maps.
+	 * 
 	 * @throws IllegalStateException
 	 *             If the MapManager is already initialized.
 	 */
-	public static void init() {
+	public static void init(File rootDirectory) {
 		if (instance != null) {
 			throw new IllegalStateException("Already initialized!");
 		}
-		instance = new MapManager();
+		instance = new MapManager(rootDirectory);
 	}
 
 	/**
