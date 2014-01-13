c40e5658373d6bca759a9942d04768cb5f0de718
Dominic Ziegler <dominic.ziegler@student.kit.edu>
2014-01-13 19:33:40 +0100

    Dummy implementation for MapImporter
    Make MapImporter.importMap() return a simple dummy graph consisting of
    four primary streets in a rectangle, one of which is a oneway street.

--- src/edu/kit/pse/ws2013/routekit/precalculation/MapImporter.java
+++ src/edu/kit/pse/ws2013/routekit/precalculation/MapImporter.java
@@ -1,26 +1,79 @@
 package edu.kit.pse.ws2013.routekit.precalculation;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import org.xml.sax.SAXException;
 
+import edu.kit.pse.ws2013.routekit.map.EdgeBasedGraph;
+import edu.kit.pse.ws2013.routekit.map.EdgeProperties;
+import edu.kit.pse.ws2013.routekit.map.Graph;
+import edu.kit.pse.ws2013.routekit.map.HighwayType;
+import edu.kit.pse.ws2013.routekit.map.NodeProperties;
 import edu.kit.pse.ws2013.routekit.map.StreetMap;
+import edu.kit.pse.ws2013.routekit.map.TurnType;
+import edu.kit.pse.ws2013.routekit.map.Restriction;
 
 /**
- * Stellt die Funktionalität zum Importieren einer neuen Karte bereit.
+ * This class provides the functionality to import a new map.
  */
 public class MapImporter {
 	/**
-	 * Importiert eine neue Karte aus der angegebenen OpenStreetMap-Datei. Die
-	 * vom {@link OSMParser} aufgebaute Graphdatenstruktur wird dabei vom
-	 * {@link GraphPartitioner} partitioniert und zurückgegeben.
+	 * Imports a new map from the specified OpenStreetMap data file. The graph
+	 * data structure built by the {@link OSMParser} is partitioned using a
+	 * {@link GraphPartitioner} and then returned.
 	 * 
 	 * @param file
-	 *            Die OpenStreetMap-Datei, aus der die Kartendaten importiert
-	 *            werden sollen.
+	 *            the OSM file from which the map data is to be imported
 	 * @param name
-	 *            Der Name der neuen {@link StreetMap}.
-	 * @return
+	 *            the name of the new {@link StreetMap}
+	 * @return the imported {@link StreetMap}
+	 * @throws IllegalArgumentException
+	 *             if {@code file} is {@code null}
+	 * @throws IOException
+	 *             if any I/O error occurs
+	 * @throws SAXException
+	 *             if an error occurs during parsing the OSM file, e.g. if the
+	 *             file format is invalid
 	 */
-	public StreetMap importMap(File file, String name) {
-		return null;
+	public StreetMap importMap(File file, String name) throws IOException,
+			SAXException {
+		if (file == null) {
+			throw new IllegalArgumentException();
+		}
+
+		// TODO: dummy implementation
+		int nodes[] = { 0, 2, 4, 6 };
+		int edges[] = { 1, 2, 0, 3, 0, 3, 1 };
+
+		float lat[] = { 49.002168F, 49.002238F, 49.001506F, 49.001562F };
+		float lon[] = { 8.405803F, 8.407928F, 8.405761F, 8.407928F };
+
+		EdgeProperties theProp = new EdgeProperties(HighwayType.Primary,
+				"Ringstraße", "B 42", 0);
+		EdgeProperties props[] = new EdgeProperties[edges.length];
+		Arrays.fill(props, theProp);
+
+		Graph graph = new Graph(nodes, edges,
+				new HashMap<Integer, NodeProperties>(), props, lat, lon);
+
+		EdgeBasedGraph edgeGraph = createDummyEdgedBasedGraph();
+		int partitions[] = { 1, 1, 1, 1 };
+		edgeGraph.setPartitions(partitions);
+
+		StreetMap map = new StreetMap(graph, edgeGraph);
+		map.setName(name);
+		return map;
+	}
+
+	private EdgeBasedGraph createDummyEdgedBasedGraph() {
+		int edges[] = { 0, 1, 2, 3, 3, 5, 6 };
+		int turns[] = { 3, 5, 1, 0, 6, 2 };
+		TurnType types[] = { TurnType.RightTurn, TurnType.LeftTurn,
+				TurnType.LeftTurn, TurnType.RightTurn, TurnType.LeftTurn,
+				TurnType.LeftTurn };
+		return new EdgeBasedGraph(edges, turns, types,
+				new HashMap<Integer, Restriction>());
 	}
 }
