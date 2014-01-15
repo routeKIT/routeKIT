2d1bfb64dcacda4bb34590a678240d94f8b135e6
Dominic Ziegler <dominic.ziegler@student.kit.edu>
2014-01-15 19:35:33 +0100

    Implement OSMWay, leaving dummy implementation for getRestriction()

--- src/edu/kit/pse/ws2013/routekit/precalculation/OSMWay.java
+++ src/edu/kit/pse/ws2013/routekit/precalculation/OSMWay.java
@@ -3,6 +3,7 @@ package edu.kit.pse.ws2013.routekit.precalculation;
 import java.util.Map;
 
 import edu.kit.pse.ws2013.routekit.map.EdgeProperties;
+import edu.kit.pse.ws2013.routekit.map.HighwayType;
 import edu.kit.pse.ws2013.routekit.map.Restriction;
 
 /**
@@ -10,6 +11,8 @@ import edu.kit.pse.ws2013.routekit.map.Restriction;
  * {@link OSMParser}.
  */
 public class OSMWay {
+	private Map<String, String> props;
+
 	/**
 	 * Creates a new object from the given OSM tags.
 	 * 
@@ -17,6 +20,7 @@ public class OSMWay {
 	 *            a list of OSM tags (name/value pairs)
 	 */
 	public OSMWay(Map<String, String> props) {
+		this.props = props;
 	}
 
 	/**
@@ -32,10 +36,11 @@ public class OSMWay {
 	/**
 	 * Determines if this way is (part of) a roundabout.
 	 * 
-	 * @return {@code} if it is a roundabout, otherwise {@code false}
+	 * @return {@code true} if it is a roundabout, otherwise {@code false}
 	 */
 	public boolean isRoundabout() {
-		return false;
+		return props.containsKey("junction")
+				&& props.get("junction").equals("roundabout");
 	}
 
 	/**
@@ -45,7 +50,54 @@ public class OSMWay {
 	 *         it can be used in both directions
 	 */
 	public boolean isOneway() {
-		return false;
+		if (!props.containsKey("oneway")) {
+			return false;
+		}
+		switch (props.get("oneway").toLowerCase()) {
+		case "yes":
+		case "true":
+		case "1":
+			return true;
+		case "no":
+		case "false":
+		case "0":
+			return false;
+		default:
+			return isRoundabout() || getHighwayType() == HighwayType.Motorway;
+		}
+	}
+
+	/**
+	 * Determines if this way is a (relevant) highway and, if yes, what type it
+	 * is.
+	 * 
+	 * @return the highway type of this way, or {@code null} if no highway
+	 */
+	public HighwayType getHighwayType() {
+		switch (props.get("highway")) {
+		case "motorway":
+		case "motorway_link":
+			return HighwayType.Motorway;
+		case "trunk":
+		case "trunk_link":
+			return HighwayType.Trunk;
+		case "primary":
+		case "primary_link":
+			return HighwayType.Primary;
+		case "secondary":
+		case "secondary_link":
+			return HighwayType.Secondary;
+		case "tertiary":
+		case "tertiary_link":
+			return HighwayType.Tertiary;
+		case "unclassified":
+		case "road":
+			return HighwayType.Unclassified;
+		case "residential":
+			return HighwayType.Residential;
+		default:
+			return null;
+		}
 	}
 
 	/**
@@ -55,6 +107,22 @@ public class OSMWay {
 	 * @return the said properties object
 	 */
 	public EdgeProperties getEdgeProperties() {
-		return null;
+		int maxSpeed;
+		try {
+			maxSpeed = Math.max(Integer.parseInt(props.get("maxspeed")), 0);
+		} catch (NumberFormatException e) {
+			maxSpeed = 0;
+		}
+
+		String name = props.get("name");
+		if (name != null && name.isEmpty()) {
+			name = null;
+		}
+		String ref = props.get("ref");
+		if (ref != null && ref.isEmpty()) {
+			ref = null;
+		}
+
+		return new EdgeProperties(getHighwayType(), name, ref, maxSpeed);
 	}
 }
