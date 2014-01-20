533eac23a934623fa159e2856cf9050ca7ec50a6
Dominic Ziegler <dominic.ziegler@student.kit.edu>
2014-01-20 17:32:01 +0100

    Implement OSMWay.getRestrictions() for "number" restrictions
    VehicleTypeRestriction still missing

--- src/edu/kit/pse/ws2013/routekit/precalculation/OSMWay.java
+++ src/edu/kit/pse/ws2013/routekit/precalculation/OSMWay.java
@@ -1,26 +1,32 @@
 package edu.kit.pse.ws2013.routekit.precalculation;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import edu.kit.pse.ws2013.routekit.map.EdgeProperties;
+import edu.kit.pse.ws2013.routekit.map.HeightRestriction;
 import edu.kit.pse.ws2013.routekit.map.HighwayType;
+import edu.kit.pse.ws2013.routekit.map.MultipleRestrictions;
 import edu.kit.pse.ws2013.routekit.map.Restriction;
+import edu.kit.pse.ws2013.routekit.map.WeightRestriction;
+import edu.kit.pse.ws2013.routekit.map.WidthRestriction;
 
 /**
  * A way in an OpenStreetMap file. This is only a helper class used by the
  * {@link OSMParser}.
  */
 public class OSMWay {
-	private Map<String, String> props;
+	private Map<String, String> tags;
 
 	/**
 	 * Creates a new object from the given OSM tags.
 	 * 
-	 * @param props
+	 * @param tags
 	 *            a list of OSM tags (name/value pairs)
 	 */
-	public OSMWay(Map<String, String> props) {
-		this.props = props;
+	public OSMWay(Map<String, String> tags) {
+		this.tags = tags;
 	}
 
 	/**
@@ -30,7 +36,33 @@ public class OSMWay {
 	 * @return the restriction(s) of this way, or {@code null} if none
 	 */
 	public Restriction getRestriction() {
-		return null;
+		List<Restriction> restrictions = new ArrayList<>();
+		if (tags.containsKey("maxweight")) {
+			try {
+				int maxWeight = (int) Float.parseFloat(tags.get("maxweight")) * 1000;
+				restrictions.add(WeightRestriction.getInstance(maxWeight));
+			} catch (NumberFormatException e) {
+				// Ignore invalid value for the maxweight tag
+			}
+		}
+		if (tags.containsKey("maxwidth")) {
+			try {
+				int maxWidth = (int) Float.parseFloat(tags.get("maxweight")) * 100;
+				restrictions.add(WidthRestriction.getInstance(maxWidth));
+			} catch (NumberFormatException e) {
+				// Ignore invalid value for the maxwidth tag
+			}
+		}
+		if (tags.containsKey("maxheight")) {
+			try {
+				int maxHeight = (int) Float.parseFloat(tags.get("maxheight")) * 100;
+				restrictions.add(HeightRestriction.getInstance(maxHeight));
+			} catch (NumberFormatException e) {
+				// Ignore invalid value for the maxheight tag
+			}
+		}
+		// TODO: vehicle type restrictions missing
+		return MultipleRestrictions.getInstance(restrictions);
 	}
 
 	/**
@@ -39,8 +71,8 @@ public class OSMWay {
 	 * @return {@code true} if it is a roundabout, otherwise {@code false}
 	 */
 	public boolean isRoundabout() {
-		return props.containsKey("junction")
-				&& props.get("junction").equals("roundabout");
+		return tags.containsKey("junction")
+				&& tags.get("junction").equals("roundabout");
 	}
 
 	/**
@@ -50,10 +82,10 @@ public class OSMWay {
 	 *         it can also be used in the opposite direction
 	 */
 	public boolean isOneway() {
-		if (!props.containsKey("oneway")) {
+		if (!tags.containsKey("oneway")) {
 			return false;
 		}
-		switch (props.get("oneway").toLowerCase()) {
+		switch (tags.get("oneway").toLowerCase()) {
 		case "yes":
 		case "true":
 		case "1":
@@ -76,7 +108,7 @@ public class OSMWay {
 	 *         direction, or {@code false} if not
 	 */
 	public boolean isReversedOneway() {
-		return props.containsKey("oneway") && props.get("oneway").equals("-1");
+		return tags.containsKey("oneway") && tags.get("oneway").equals("-1");
 	}
 
 	/**
@@ -86,11 +118,10 @@ public class OSMWay {
 	 * @return the highway type of this way, or {@code null} if no highway
 	 */
 	public HighwayType getHighwayType() {
-		String string = props.get("highway");
-		if (string == null) {
+		if (!tags.containsKey("highway")) {
 			return null;
 		}
-		switch (string) {
+		switch (tags.get("highway")) {
 		case "motorway":
 		case "motorway_link":
 			return HighwayType.Motorway;
@@ -125,16 +156,16 @@ public class OSMWay {
 	public EdgeProperties getEdgeProperties() {
 		int maxSpeed;
 		try {
-			maxSpeed = Math.max(Integer.parseInt(props.get("maxspeed")), 0);
+			maxSpeed = Math.max(Integer.parseInt(tags.get("maxspeed")), 0);
 		} catch (NumberFormatException e) {
 			maxSpeed = 0;
 		}
 
-		String name = props.get("name");
+		String name = tags.get("name");
 		if (name != null && name.isEmpty()) {
 			name = null;
 		}
-		String ref = props.get("ref");
+		String ref = tags.get("ref");
 		if (ref != null && ref.isEmpty()) {
 			ref = null;
 		}
