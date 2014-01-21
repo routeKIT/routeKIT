1f2873661261617c71d5cd7d599640e5aa859941
Anastasia Zinkina <uajzt@student.kit.edu>
2014-01-21 11:56:42 +0100

    Rendern: Grenzen.

--- src/edu/kit/pse/ws2013/routekit/util/Coordinates.java
+++ src/edu/kit/pse/ws2013/routekit/util/Coordinates.java
@@ -18,7 +18,7 @@ public class Coordinates {
 	 *             if the coordinates are out of range
 	 */
 	public Coordinates(float lat, float lon) {
-		if (lat < -86 || lat > 86 || lon <= -181 || lon >= 181) {
+		if (lat < -90 || lat > 90 || lon <= -181 || lon >= 181) {
 			throw new IllegalArgumentException();
 		}
 
