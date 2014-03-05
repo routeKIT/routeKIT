87eac8193227bb010a8c5f7bcae44c3bc1e950e7
Felix Dörre <felix.doerre@student.kit.edu>
2014-03-05 12:46:05 +0100

    Qual: more exact CLI test.

--- test/edu/kit/pse/ws2013/routekit/controllers/TestCLI.java
+++ test/edu/kit/pse/ws2013/routekit/controllers/TestCLI.java
@@ -1,18 +1,48 @@
 package edu.kit.pse.ws2013.routekit.controllers;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.net.URL;
+import java.net.URLClassLoader;
 
 import org.junit.Assert;
 import org.junit.Test;
+
+import edu.kit.pse.ws2013.routekit.Resources;
 
 public class TestCLI {
 
 	private boolean fail = false;
 
+	private Runnable testCLI(String[] args) throws IOException,
+			ReflectiveOperationException {
+		ClassLoader c = new URLClassLoader(new URL[] { new File("bin").toURI()
+				.toURL() }, TestCLI.class.getClassLoader().getParent());
+		Class<Runnable> cli = (Class<Runnable>) c
+				.loadClass("edu.kit.pse.ws2013.routekit.controllers.CLI");
+		return cli.getConstructor(String[].class).newInstance((Object) args);
+	}
+
 	@Test
-	public void testPrecalculate() throws IOException, InterruptedException {
+	public void testPrecalculate() throws IOException, InterruptedException,
+			ReflectiveOperationException {
+		runCLI("--import", "testmap1", Resources.getKarlsruheBigLocation());
+		runCLI("--precalculate", "testmap1", "PKW (Standard)");
+		System.gc();
+		System.gc();
+		System.gc();
+		// if (true) {
+		// return;
+		// }
+		runCLI("--delete-precalculation", "testmap1", "PKW (Standard)",
+				"--delete-map", "testmap1");
+
+	}
+
+	private void runCLI(String... args) throws IOException,
+			ReflectiveOperationException, InterruptedException {
 		final PrintStream err = System.err;
 		final PrintStream out = System.out;
 		try (PrintStream newErr = new PrintStream(new OutputStream() {
@@ -30,13 +60,7 @@ public class TestCLI {
 		})) {
 			System.setErr(newErr);
 			System.setOut(newOut);
-			Thread t = new Thread(new CLI(new String[] { "--precalculate",
-					"Regierungsbezirk Karlsruhe", "PKW (Standard)" }));
-			t.start();
-			Thread.sleep(1000);
-			t.stop(); // yes, I know, leave me alone
-			t.join(); // should be noop, but isn’t because precalculation is
-						// multithreaded
+			testCLI(args).run();
 		}
 		System.setErr(err);
 		System.setOut(out);
