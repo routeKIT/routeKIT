187b4c2be4ef01d9031e9f7566ab036bdc77b096
Lucas Werkmeister <lucas.werkmeister@student.kit.edu>
2014-03-05 19:50:35 +0100

    Qual: Resources: download karlsruhe_big.osm if needed

--- test/edu/kit/pse/ws2013/routekit/Resources.java
+++ test/edu/kit/pse/ws2013/routekit/Resources.java
@@ -1,9 +1,41 @@
 package edu.kit.pse.ws2013.routekit;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.Authenticator;
+import java.net.MalformedURLException;
+import java.net.PasswordAuthentication;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 
 public class Resources {
 	public static String getKarlsruheBigLocation() {
-		return new File("karlsruhe_big.osm").getAbsolutePath();
+		File file = new File("karlsruhe_big.osm").getAbsoluteFile();
+		if (!file.exists()) {
+			try {
+				URL website = new URL(
+						"http://algo2.iti.kit.edu/documents/PSE_WS1314/karlsruhe_big.osm");
+				Authenticator.setDefault(new Authenticator() {
+					@Override
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication("pse_ws1314",
+								"pse_student_ws1314".toCharArray());
+					}
+				});
+				try (ReadableByteChannel rbc = Channels.newChannel(website
+						.openStream());
+						FileOutputStream fos = new FileOutputStream(file);) {
+					fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			} catch (MalformedURLException e) {
+				// won’t happen, hardcoded URL
+				e.printStackTrace();
+			}
+		}
+		return file.getPath();
 	}
 }
