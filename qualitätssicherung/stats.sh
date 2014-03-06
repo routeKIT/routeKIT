05ea4c27f1a1b17a06fe2914cda9f56b93ad8bd2
Lucas Werkmeister <lucas.werkmeister@student.kit.edu>
2014-03-06 16:15:22 +0100

    Qual: Stats: reindent

--- qualitätssicherung/stats.sh
+++ qualitätssicherung/stats.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 function output {
-  echo "in $1"
-  echo "Alle Zeilen: $2"
-  echo "keine Leerzeilen: $3"
-  echo "keine Leerzeilen, Kommentare, imports oder Zeilen nur mit schliessender Klammer: $4"
-   
+    echo "in $1"
+    echo "Alle Zeilen: $2"
+    echo "keine Leerzeilen: $3"
+    echo "keine Leerzeilen, Kommentare, imports oder Zeilen nur mit schliessender Klammer: $4"
 }
 
 pushd ..> /dev/null
@@ -13,20 +12,20 @@ SUM_EMPTY=0
 SUM_CLEAN=0
 
 for part in src test; do
-T_ALL="$(find $part -name '*.java' -exec cat {} \+)"
-T_NO_EMPTY="$(grep -v '^$' <<< "$T_ALL")"
-T_CLEAN="$(grep -v '^import .*;$' <<< "$T_NO_EMPTY" | grep -v '^[[:space:]]*}[[:space:]]*$' | grep -v '^[[:space:]]*\(//\|/\?\*\).*$')"
-
-ALL=$(wc -l <<< "$T_ALL")
-NO_EMPTY=$(wc -l <<< "$T_NO_EMPTY")
-CLEAN=$(wc -l <<< "$T_CLEAN")
-
-output $part $ALL $NO_EMPTY $CLEAN
-echo
-
-SUM=$(($SUM+$ALL))
-SUM_EMPTY=$(($SUM_EMPTY+$NO_EMPTY))
-SUM_CLEAN=$(($SUM_CLEAN+$CLEAN))
+    T_ALL="$(find $part -name '*.java' -exec cat {} \+)"
+    T_NO_EMPTY="$(grep -v '^$' <<< "$T_ALL")"
+    T_CLEAN="$(grep -v '^import .*;$' <<< "$T_NO_EMPTY" | grep -v '^[[:space:]]*}[[:space:]]*$' | grep -v '^[[:space:]]*\(//\|/\?\*\).*$')"
+    
+    ALL=$(wc -l <<< "$T_ALL")
+    NO_EMPTY=$(wc -l <<< "$T_NO_EMPTY")
+    CLEAN=$(wc -l <<< "$T_CLEAN")
+    
+    output $part $ALL $NO_EMPTY $CLEAN
+    echo
+    
+    SUM=$(($SUM+$ALL))
+    SUM_EMPTY=$(($SUM_EMPTY+$NO_EMPTY))
+    SUM_CLEAN=$(($SUM_CLEAN+$CLEAN))
 done
 
 output Gesamt $SUM $SUM_EMPTY $SUM_CLEAN
