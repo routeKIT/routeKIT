2c2305f69ba46f77152cc264515b50106fc0619f
Felix Dörre <felix.doerre@student.kit.edu>
2014-03-06 15:59:06 +0100

    Qual: stats-script do output function

--- qualitätssicherung/stats.sh
+++ qualitätssicherung/stats.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+function output {
+  echo "in $1"
+  echo "Alle Zeilen: $2"
+  echo "keine Leerzeilen: $3"
+  echo "keine Leerzeilen, Kommentare, imports oder Zeilen nur mit schliessender Klammer: $4"
+   
+}
+
 pushd ..> /dev/null
 SUM=0
 SUM_EMPTY=0
@@ -9,10 +17,7 @@ ALL=`find $part -name '*.java' -exec cat {} \+ | wc -l`
 NO_EMPTY=`find $part -name '*.java' -exec cat {} \+ | grep -v '^$' | wc -l`
 CLEAN=`find $part -name '*.java' -exec cat {} \+ | grep -v '^$' | grep -v '^import .*;$' | grep -v '^[[:space:]]*}[[:space:]]*$'  | grep -v '^[[:space:]]*\(//\|/\?\*\).*$' | wc -l`
 
-echo "in $part"
-echo "Alle Zeilen: $ALL"
-echo "keine Leerzeilen: $NO_EMPTY"
-echo "keine Leerzeilen, Kommentare, imports oder Zeilen nur mit schliessender Klammer: $CLEAN"
+output $part $ALL $NO_EMPTY $CLEAN
 echo
 
 SUM=$(($SUM+$ALL))
@@ -20,11 +25,6 @@ SUM_EMPTY=$(($SUM_EMPTY+$NO_EMPTY))
 SUM_CLEAN=$(($SUM_CLEAN+$CLEAN))
 done
 
-echo "gesamt:"
-
-echo "Alle Zeilen: $SUM"
-echo "keine Leerzeilen: $SUM_EMPTY"
-echo "keine Leerzeilen, Kommentare, imports oder Zeilen nur mit schliessender Klammer: $SUM_CLEAN"
-
+output Gesamt $SUM $SUM_EMPTY $SUM_CLEAN
 
 popd > /dev/null
