cc3f84e12784f4a0fc39425d7efe97530bb56388
Felix Dörre <felix.doerre@student.kit.edu>
2014-03-06 15:56:35 +0100

    Qual: stats-script: bash-arithmetic-summary

--- qualitätssicherung/stats.sh
+++ qualitätssicherung/stats.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 pushd ..> /dev/null
+SUM=0
+SUM_EMPTY=0
+SUM_CLEAN=0
 
 for part in src test; do
+ALL=`find $part -name '*.java' -exec cat {} \+ | wc -l`
+NO_EMPTY=`find $part -name '*.java' -exec cat {} \+ | grep -v '^$' | wc -l`
+CLEAN=`find $part -name '*.java' -exec cat {} \+ | grep -v '^$' | grep -v '^import .*;$' | grep -v '^[[:space:]]*}[[:space:]]*$'  | grep -v '^[[:space:]]*\(//\|/\?\*\).*$' | wc -l`
+
 echo "in $part"
-echo -n "Alle Zeilen: "
-find $part -name '*.java' -exec cat {} \+ | wc -l
-echo -n "keine Leerzeilen: "
-find $part -name '*.java' -exec cat {} \+ | grep -v '^$' | wc -l
-echo -n "keine Leerzeilen, Kommentare, imports oder Zeilen nur mit schliessender Klammer: "
-find $part -name '*.java' -exec cat {} \+ | grep -v '^$' | grep -v '^import .*;$' | grep -v '^[[:space:]]*}[[:space:]]*$'  | grep -v '^[[:space:]]*\(//\|/\?\*\).*$' | wc -l
+echo "Alle Zeilen: $ALL"
+echo "keine Leerzeilen: $NO_EMPTY"
+echo "keine Leerzeilen, Kommentare, imports oder Zeilen nur mit schliessender Klammer: $CLEAN"
 echo
+
+SUM=$(($SUM+$ALL))
+SUM_EMPTY=$(($SUM_EMPTY+$NO_EMPTY))
+SUM_CLEAN=$(($SUM_CLEAN+$CLEAN))
 done
+
+echo "gesamt:"
+
+echo "Alle Zeilen: $SUM"
+echo "keine Leerzeilen: $SUM_EMPTY"
+echo "keine Leerzeilen, Kommentare, imports oder Zeilen nur mit schliessender Klammer: $SUM_CLEAN"
+
 
 popd > /dev/null
